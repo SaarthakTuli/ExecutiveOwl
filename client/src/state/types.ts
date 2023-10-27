d340e8056c8384c13a4b2d0e8f578981b8110b47
SaarthakTuli <saarthaktuli@gmail.com>
2023-10-27 23:11:27 +0530

    init

---
+++ client/src/state/types.ts
@@ -0,0 +1,58 @@
+export interface ExpensesByCategory {
+    salaries: number;
+    supplies: number;
+    services: number;
+}
+
+export interface MonthlyData {
+    id: string;
+    month: string;
+    
+    revenue: number;
+    expenses: number;
+    operationalExpenses: number;
+    nonOperationalExpenses: number;
+}
+
+export interface DailyData {
+    id: string;
+    date: string;
+    revenue: number;
+    expense: number;
+}
+
+export interface GetKpisResponse {
+    id: string;
+    __id: string;
+    __v: number;
+
+    totalProfit: number;
+    totalRevenue: number;
+    totalExpenses: number;
+
+    expensesByCategory: ExpensesByCategory,
+    monthlyData: Array<MonthlyData>,
+    dailyData: Array<DailyData>
+}
+
+export interface GetProductsResponse {
+    id: string;
+    _id: string;
+    __v: number;
+    price: number;
+    expense: number;
+    transactions: Array<string>;
+    createdAt: string;
+    updatedAt: string;
+  }
+  
+  export interface GetTransactionsResponse {
+    id: string;
+    _id: string;
+    __v: number;
+    buyer: string;
+    amount: number;
+    productIds: Array<string>;
+    createdAt: string;
+    updatedAt: string;
+  }
